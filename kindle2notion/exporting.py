19041a2e33a9d1c82aefb85234c65fc237917992
b.saintot <bsaintot@octo.com>
2021-05-16 17:34:47 +0200

    Remove notion client from settings.py

--- kindle2notion/exporting.py
+++ kindle2notion/exporting.py
@@ -1,15 +1,20 @@
 from datetime import datetime
 
 from notion.block import TextBlock
+from notion.client import NotionClient
 from notion.collection import NotionDate
 
-from settings import ENABLE_HIGHLIGHT_DATE, ENABLE_BOOK_COVER, client, notion_collection_view, \
-    notion_collection_view_rows
+from settings import ENABLE_HIGHLIGHT_DATE, ENABLE_BOOK_COVER, NOTION_TOKEN, NOTION_TABLE_ID
 from utilities import getBookCoverURI, NO_COVER_IMG, ITALIC
+
+NOTION_CLIENT = NotionClient(token_v2=NOTION_TOKEN)
+NOTION_COLLECTION_VIEW = NOTION_CLIENT.get_collection_view(NOTION_TABLE_ID)
+NOTION_COLLECTION_VIEW_ROWS = NOTION_COLLECTION_VIEW.collection.get_rows()
 
 
 def export_to_notion(books):
     print('Initiating transfer...\n')
+
     for title in books:
         book = books[title]
         author = book['author']
@@ -43,11 +48,11 @@ def prepare_aggregated_text_for_one_book(highlights):
     return aggregated_text, last_date
 
 
-def add_book_to_notion(title, author, highlightCount, aggregatedText, lastNoteDate):
+def add_book_to_notion(title, author, highlight_count, aggregated_text, last_date):
     title_exists = False
 
-    if notion_collection_view_rows != []:
-        for each_row in notion_collection_view_rows:
+    if NOTION_COLLECTION_VIEW_ROWS:
+        for each_row in NOTION_COLLECTION_VIEW_ROWS:
             # to account for the use-case of books with the same name by different authors
             if title == each_row.title and author == each_row.author:
                 title_exists = True
@@ -55,7 +60,7 @@ def add_book_to_notion(title, author, highlightCount, aggregatedText, lastNoteDa
 
                 if row.highlights == None:
                     row.highlights = 0  # to initialize number of highlights as 0
-                elif row.highlights == highlightCount:  # if no change in highlights
+                elif row.highlights == highlight_count:  # if no change in highlights
                     return ('None to add')
 
     title_and_author = title + ' (' + str(author) + ')'
@@ -63,7 +68,7 @@ def add_book_to_notion(title, author, highlightCount, aggregatedText, lastNoteDa
     print('-' * len(title_and_author))
 
     if not title_exists:
-        row = notion_collection_view.collection.add_row()
+        row = NOTION_COLLECTION_VIEW.collection.add_row()
         row.title = title
         row.author = author
         row.highlights = 0
@@ -79,15 +84,15 @@ def add_book_to_notion(title, author, highlightCount, aggregatedText, lastNoteDa
                 print(
                     '× Book cover couldn\'t be found. Please replace the placeholder image with the original book cover manually')
 
-    parent_page = client.get_block(row.id)
+    parent_page = NOTION_CLIENT.get_block(row.id)
 
     # For existing books with new highlights to add
     for all_blocks in parent_page.children:
         all_blocks.remove()
-    parent_page.children.add_new(TextBlock, title=aggregatedText)
-    diffCount = highlightCount - row.highlights
-    row.highlights = highlightCount
-    row.last_highlighted = NotionDate(lastNoteDate)
+    parent_page.children.add_new(TextBlock, title=aggregated_text)
+    diffCount = highlight_count - row.highlights
+    row.highlights = highlight_count
+    row.last_highlighted = NotionDate(last_date)
     row.last_synced = NotionDate(datetime.now())
     message = str(diffCount) + ' notes / highlights added successfully\n'
     return (message)
