8c99546a3af96b6d5c75a894676c0f2ae505d98b
Adam Tokarski <adam.tokarski@stxnext.pl>
2021-05-31 13:54:30 +0200

    [issue_35] reformatted a little after running black

--- kindle2notion/exporting.py
+++ kindle2notion/exporting.py
@@ -7,54 +7,79 @@ from notion.client import NotionClient
 from notion.collection import NotionDate
 from requests import get
 
-NO_COVER_IMG = 'https://via.placeholder.com/150x200?text=No%20Cover'
-ITALIC = '*'
+NO_COVER_IMG = "https://via.placeholder.com/150x200?text=No%20Cover"
+ITALIC = "*"
 
 
 # TODO: Refactor this module
 
 
-def export_to_notion(books: Dict, enable_highlight_date: bool, enable_book_cover: bool, notion_token: str,
-                     notion_table_id: str) -> None:
-    print('Initiating transfer...\n')
+def export_to_notion(
+    books: Dict,
+    enable_highlight_date: bool,
+    enable_book_cover: bool,
+    notion_token: str,
+    notion_table_id: str,
+) -> None:
+    print("Initiating transfer...\n")
 
     for title in books:
         book = books[title]
-        author = book['author']
-        highlights = book['highlights']
+        author = book["author"]
+        highlights = book["highlights"]
         highlight_count = len(highlights)
-        aggregated_text_from_highlights, last_date = _prepare_aggregated_text_for_one_book(highlights,
-                                                                                           enable_highlight_date)
+        (
+            aggregated_text_from_highlights,
+            last_date,
+        ) = _prepare_aggregated_text_for_one_book(highlights, enable_highlight_date)
 
-        message = _add_book_to_notion(title, author, highlight_count, aggregated_text_from_highlights, last_date,
-                                      notion_token, notion_table_id, enable_book_cover)
-        if message != 'None to add':
-            print('✓', message)
+        message = _add_book_to_notion(
+            title,
+            author,
+            highlight_count,
+            aggregated_text_from_highlights,
+            last_date,
+            notion_token,
+            notion_table_id,
+            enable_book_cover,
+        )
+        if message != "None to add":
+            print("✓", message)
 
 
-def _prepare_aggregated_text_for_one_book(highlights: List, enable_highlight_date: bool) -> Tuple[str, str]:
-    aggregated_text = ''
+def _prepare_aggregated_text_for_one_book(
+    highlights: List, enable_highlight_date: bool
+) -> Tuple[str, str]:
+    aggregated_text = ""
     for highlight in highlights:
         text = highlight[0]
         page = highlight[1]
         location = highlight[2]
         date = highlight[3]
 
-        aggregated_text += text + '\n('
-        if page != '':
-            aggregated_text += ITALIC + 'Page: ' + page + ITALIC + '  '
-        if location != '':
-            aggregated_text += ITALIC + 'Location: ' + location + ITALIC + '  '
-        if enable_highlight_date and (date != ''):
-            aggregated_text += ITALIC + 'Date Added: ' + date + ITALIC
+        aggregated_text += text + "\n("
+        if page != "":
+            aggregated_text += ITALIC + "Page: " + page + ITALIC + "  "
+        if location != "":
+            aggregated_text += ITALIC + "Location: " + location + ITALIC + "  "
+        if enable_highlight_date and (date != ""):
+            aggregated_text += ITALIC + "Date Added: " + date + ITALIC
 
-        aggregated_text = aggregated_text.strip() + ')\n\n'
+        aggregated_text = aggregated_text.strip() + ")\n\n"
     last_date = date
     return aggregated_text, last_date
 
 
-def _add_book_to_notion(title: str, author: str, highlight_count: int, aggregated_text: str, last_date: str,
-                        notion_token: str, notion_table_id: str, enable_book_cover: bool) -> str:
+def _add_book_to_notion(
+    title: str,
+    author: str,
+    highlight_count: int,
+    aggregated_text: str,
+    last_date: str,
+    notion_token: str,
+    notion_table_id: str,
+    enable_book_cover: bool,
+) -> str:
     notion_client = NotionClient(token_v2=notion_token)
     notion_collection_view = notion_client.get_collection_view(notion_table_id)
     notion_collection_view_rows = notion_collection_view.collection.get_rows()
@@ -69,11 +94,11 @@ def _add_book_to_notion(title: str, author: str, highlight_count: int, aggregate
                 if row.highlights is None:
                     row.highlights = 0  # to initialize number of highlights as 0
                 elif row.highlights == highlight_count:  # if no change in highlights
-                    return 'None to add'
+                    return "None to add"
 
-    title_and_author = title + ' (' + str(author) + ')'
+    title_and_author = title + " (" + str(author) + ")"
     print(title_and_author)
-    print('-' * len(title_and_author))
+    print("-" * len(title_and_author))
 
     if not title_exists:
         row = notion_collection_view.collection.add_row()
@@ -86,11 +111,13 @@ def _add_book_to_notion(title: str, author: str, highlight_count: int, aggregate
                 result = _get_book_cover_uri(row.title, row.author)
             if result is not None:
                 row.cover = result
-                print('✓ Added book cover')
+                print("✓ Added book cover")
             else:
                 row.cover = NO_COVER_IMG
-                print('× Book cover couldn\'t be found. '
-                      'Please replace the placeholder image with the original book cover manually.')
+                print(
+                    "× Book cover couldn't be found. "
+                    "Please replace the placeholder image with the original book cover manually."
+                )
 
     parent_page = notion_client.get_block(row.id)
 
@@ -102,22 +129,22 @@ def _add_book_to_notion(title: str, author: str, highlight_count: int, aggregate
     row.highlights = highlight_count
     row.last_highlighted = NotionDate(parse(last_date))
     row.last_synced = NotionDate(datetime.now())
-    message = str(diff_count) + ' notes / highlights added successfully\n'
+    message = str(diff_count) + " notes / highlights added successfully\n"
     return message
 
 
 def _get_book_cover_uri(title: str, author: str):
-    req_uri = 'https://www.googleapis.com/books/v1/volumes?q='
+    req_uri = "https://www.googleapis.com/books/v1/volumes?q="
 
     if title is None:
         return
-    req_uri += 'intitle:' + title
+    req_uri += "intitle:" + title
 
     if author is not None:
-        req_uri += '+inauthor:' + author
+        req_uri += "+inauthor:" + author
 
-    response = get(req_uri).json().get('items', [])
+    response = get(req_uri).json().get("items", [])
     if len(response) > 0:
-        return response[0].get('volumeInfo', {}).get('imageLinks', {}).get('thumbnail')
+        return response[0].get("volumeInfo", {}).get("imageLinks", {}).get("thumbnail")
 
     return
--- kindle2notion/parsing.py
+++ kindle2notion/parsing.py
@@ -5,25 +5,27 @@ from dateparser import parse
 
 
 def parse_raw_clippings_text(raw_clippings_text: str) -> Dict:
-    raw_clippings_list = raw_clippings_text.split('==========')
-    print(f'Found {len(raw_clippings_list)} notes and highlights.\n')
+    raw_clippings_list = raw_clippings_text.split("==========")
+    print(f"Found {len(raw_clippings_list)} notes and highlights.\n")
 
     books = {}
     passed_clippings_count = 0
 
     for raw_clipping in raw_clippings_list:
-        raw_clipping_list = raw_clipping.strip().split('\n')
+        raw_clipping_list = raw_clipping.strip().split("\n")
 
         if _is_valid_clipping(raw_clipping_list):
             author, title = _parse_author_and_title(raw_clipping_list)
             page, location, date = _parse_page_location_and_date(raw_clipping_list)
             highlight = raw_clipping_list[3]
 
-            books = _add_parsed_items_to_books_dict(books, title, author, highlight, page, location, date)
+            books = _add_parsed_items_to_books_dict(
+                books, title, author, highlight, page, location, date
+            )
         else:
             passed_clippings_count += 1
 
-    print(f'× Passed {passed_clippings_count} bookmarks or unsupported clippings.\n')
+    print(f"× Passed {passed_clippings_count} bookmarks or unsupported clippings.\n")
     return books
 
 
@@ -40,61 +42,82 @@ def _parse_author_and_title(raw_clipping_list: List) -> Tuple[str, str]:
 
 def _parse_page_location_and_date(raw_clipping_list: List) -> Tuple[str, str, str]:
     second_line = raw_clipping_list[1]
-    second_line_as_list = second_line.strip().split(' | ')
-    page = location = date = ''
+    second_line_as_list = second_line.strip().split(" | ")
+    page = location = date = ""
 
     for element in second_line_as_list:
         element = element.lower()
-        if 'page' in element:
-            page = element[element.find('page'):].replace('page', '').strip()
-        if 'location' in element:
-            location = element[element.find('location'):].replace('location', '').strip()
-        if 'added on' in element:
-            date = parse(element[element.find('added on'):].replace('added on', '').strip())
-            date = date.strftime('%A, %d %B %Y %I:%M:%S %p')
+        if "page" in element:
+            page = element[element.find("page") :].replace("page", "").strip()
+        if "location" in element:
+            location = (
+                element[element.find("location") :].replace("location", "").strip()
+            )
+        if "added on" in element:
+            date = parse(
+                element[element.find("added on") :].replace("added on", "").strip()
+            )
+            date = date.strftime("%A, %d %B %Y %I:%M:%S %p")
 
     return page, location, date
 
 
-def _add_parsed_items_to_books_dict(books: Dict, title: str, author: str, highlight: str, page: str,
-                                    location: str, date: str) -> Dict:
+def _add_parsed_items_to_books_dict(
+    books: Dict,
+    title: str,
+    author: str,
+    highlight: str,
+    page: str,
+    location: str,
+    date: str,
+) -> Dict:
     if title not in books:
-        books[title] = {'author': author, 'highlights': []}
-    books[title]['highlights'].append((highlight, page, location, date))
+        books[title] = {"author": author, "highlights": []}
+    books[title]["highlights"].append((highlight, page, location, date))
     return books
 
 
 def _parse_raw_author_and_title(raw_clipping_list: List) -> Tuple[str, str]:
     author = ""
-    title = raw_clipping_list[0].replace(author, '').strip().replace(u'\ufeff', '').replace(' ()', '')
+    title = (
+        raw_clipping_list[0]
+        .replace(author, "")
+        .strip()
+        .replace("\ufeff", "")
+        .replace(" ()", "")
+    )
 
-    if (re.findall(r'\(.*?\)', raw_clipping_list[0])):
-        author = (re.findall(r'\(.*?\)', raw_clipping_list[0]))[-1]
-        author = author.removeprefix('(').removesuffix(')')
+    if re.findall(r"\(.*?\)", raw_clipping_list[0]):
+        author = (re.findall(r"\(.*?\)", raw_clipping_list[0]))[-1]
+        author = author.removeprefix("(").removesuffix(")")
     else:
-        print("For"+ title + ", No author found. You can manually add the Author details in the Notion database.")
+        print(
+            "For"
+            + title
+            + ", No author found. You can manually add the Author details in the Notion database."
+        )
     return author, title
 
 
 def _deal_with_exceptions_in_author_name(author: str, title: str) -> Tuple[str, str]:
-    if '(' in author:
-        author = author + ')'
-        title = title.removesuffix(')')
-    
-    multiple_author_delimiters = ['; ', ' & ', ' and ']
-    if ', ' in author and any(x not in author for x in multiple_author_delimiters):
-        author = " ".join(reversed(author.split(', ')))
+    if "(" in author:
+        author = author + ")"
+        title = title.removesuffix(")")
 
-    if '; ' in author:
-        authorList = author.split('; ')
-        author = ''
+    multiple_author_delimiters = ["; ", " & ", " and "]
+    if ", " in author and any(x not in author for x in multiple_author_delimiters):
+        author = " ".join(reversed(author.split(", ")))
+
+    if "; " in author:
+        authorList = author.split("; ")
+        author = ""
         for ele in authorList:
-            author += " ".join(reversed(ele.split(', '))) + ', '
-        author = author.removesuffix(', ')
+            author += " ".join(reversed(ele.split(", "))) + ", "
+        author = author.removesuffix(", ")
     return author, title
 
 
 def _deal_with_exceptions_in_title(title: str) -> str:
-    if ', The' in title:
-        title = 'The ' + title.replace(', The', '')
+    if ", The" in title:
+        title = "The " + title.replace(", The", "")
     return title
