7c83f4e1e1bb32ef4b51349750594c231c5674ab
example <alisayar0107@gmail.com>
2021-06-09 01:34:51 +0300

    errors ignore changed to xmlcharrefreplace for special characters

--- kindle2notion/reading.py
+++ kindle2notion/reading.py
@@ -4,5 +4,5 @@ from pathlib import Path
 
 def read_raw_clippings(clippings_file_path: Path) -> str:
     raw_clippings_text = open(clippings_file_path, "r", encoding="utf-8-sig").read()
-    raw_clippings_text = raw_clippings_text.encode("ascii", errors="ignore").decode()
+    raw_clippings_text = raw_clippings_text.encode("ascii", errors="xmlcharrefreplace").decode()
     return raw_clippings_text
