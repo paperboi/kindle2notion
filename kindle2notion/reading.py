2f9dfc5bc40efd33c023eb2d7456ecec235240c2
Dawid Wełna <dawidwelna@MacBook-Pro-Dawid.local>
2021-07-01 16:51:45 +0200

    fix the issue with unicode error

--- kindle2notion/reading.py
+++ kindle2notion/reading.py
@@ -1,8 +1,12 @@
-# import unicodedata
 from pathlib import Path
 
 
 def read_raw_clippings(clippings_file_path: Path) -> str:
-    raw_clippings_text = open(clippings_file_path, "r", encoding="utf-8-sig").read()
-    raw_clippings_text = raw_clippings_text.encode("ascii", errors="ignore").decode()
+    try:
+        raw_clippings_text = open(clippings_file_path, "r", encoding="utf-8-sig").read()
+        raw_clippings_text = raw_clippings_text.encode("ascii").decode()
+    except UnicodeEncodeError:
+        print('UnicodeEncodeError, encoding data with utf-8')
+        raw_clippings_text = open(clippings_file_path, "r", encoding="utf-8").read()
+        raw_clippings_text = raw_clippings_text.encode("UTF8").decode()
     return raw_clippings_text
