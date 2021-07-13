603ff4449cd14bc8c5778e850555ca9172dbfe72
Adam Tokarski <adam.tokarski@stxnext.pl>
2021-07-13 19:54:20 +0200

    [fix_tests] close clippings file after read

--- kindle2notion/reading.py
+++ kindle2notion/reading.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 
 def read_raw_clippings(clippings_file_path: Path) -> str:
-    raw_clippings_text = open(clippings_file_path, "r", encoding="utf-8-sig").read()
-    raw_clippings_text = raw_clippings_text.encode("ascii", errors="ignore").decode()
-    return raw_clippings_text
+    with open(clippings_file_path, "r", encoding="utf-8-sig") as raw_clippings_file:
+        raw_clippings_text = raw_clippings_file.read()
+    raw_clippings_text_decoded = raw_clippings_text.encode(
+        "ascii", errors="ignore"
+    ).decode()
+    return raw_clippings_text_decoded
