470002fc4fdec24bd34c7a5bae8acbbe39e78eb9
paperboi <jeffreysamjacob@gmail.com>
2022-07-14 13:07:22 +0530

    Added line to brute force remove nb spaces

--- kindle2notion/reading.py
+++ kindle2notion/reading.py
@@ -3,6 +3,7 @@ from pathlib import Path
 def read_raw_clippings(clippings_file_path: Path) -> str:
     try:
         raw_clippings_text = open(clippings_file_path, "r", encoding="utf-8-sig").read()
+        raw_clippings_text = raw_clippings_text.replace(u'\ufeff', '')
         raw_clippings_text = raw_clippings_text.encode("ascii").decode()
     except UnicodeEncodeError as e:
         print(e)
